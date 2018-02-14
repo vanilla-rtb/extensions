8bc6db0c587675975c05101c1b3efa533b6d2f7b
abushev <arseny.bushev@gmail.com>
2018-02-14 14:19:42 +0300

    NotLast

--- golang/bidder_generator.go
+++ golang/bidder_generator.go
@@ -1,33 +1,34 @@
 // The following directive is necessary to make the package coherent:
 
-// +build ignore
+// build ignore
 
 // This program generates matchers It can be invoked by running
 // go generate
 package main
 
 import (
-    "log"
-    "os"
-    "strings"
-    "path/filepath"
-    "text/template"
-    "time"
-    "reflect"
-    "fmt"
-    "io/ioutil"
-    "github.com/jessevdk/go-flags"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"text/template"
+	"time"
+
+	"github.com/jessevdk/go-flags"
 )
 
 //TODO: use reflection to create a list of fields in the template, How do we pass this struct to generator ???
-type  Domain struct {
-    name string   `cpp:"std::string"  ipc:"char_string""`
-    dom_id uint32 `cpp:"uint32_t" ipc:"uint32_t"`
+type Domain struct {
+	name   string `cpp:"std::string"  ipc:"char_string""`
+	dom_id uint32 `cpp:"uint32_t" ipc:"uint32_t"`
 }
 
 type CacheStorage struct {
-    FieldName    string
-    FieldType    string
+	FieldName    string
+	FieldType    string
 	FieldTypeIPC string
 }
 
@@ -40,7 +41,10 @@ func die(err error) {
 var funcMap = template.FuncMap{
 	"ToUpper": strings.ToUpper,
 	"ToLower": strings.ToLower,
-	"Title" : strings.Title,
+	"Title":   strings.Title,
+	"NotLast": func(x int, a interface{}) bool {
+		return x != reflect.ValueOf(a).Len()-1
+	},
 }
 
 type Options struct {
@@ -51,17 +55,16 @@ type Options struct {
 var options Options
 var parser = flags.NewParser(&options, flags.Default)
 
-
 func main() {
 
-    _, err := parser.Parse()
-    die(err)
+	_, err := parser.Parse()
+	die(err)
 
 	templateContent, err := ioutil.ReadFile(string(options.Input))
-    die(err)
+	die(err)
 	var matcherTemplate = template.Must(template.New("").Funcs(funcMap).Parse(string(templateContent)))
 
-	domain := Domain{"",0}
+	domain := Domain{"", 0}
 	v := reflect.ValueOf(domain)
 
 	cachedValues := make([]CacheStorage, v.NumField())
@@ -71,25 +74,24 @@ func main() {
 		cachedValues[i].FieldType = field.Tag.Get("cpp")
 		cachedValues[i].FieldTypeIPC = field.Tag.Get("ipc")
 		cachedValues[i].FieldName = field.Name
-		fmt.Println(cachedValues[i].FieldName , "=>" , cachedValues[i].FieldType, "=>" , cachedValues[i].FieldTypeIPC)
+		fmt.Println(cachedValues[i].FieldName, "=>", cachedValues[i].FieldType, "=>", cachedValues[i].FieldTypeIPC)
 	}
 
 	outFileName := string(options.Output)
-	generatedMatcherName  := strings.TrimSuffix(outFileName, filepath.Ext(outFileName))
-
+	generatedMatcherName := strings.TrimSuffix(outFileName, filepath.Ext(outFileName))
 
 	f, err := os.Create(outFileName)
 	die(err)
 	defer f.Close()
 
-	matcherTemplate.Execute(f, struct {
-		Timestamp   time.Time
-		Matchername string
+	err = matcherTemplate.Execute(f, struct {
+		Timestamp    time.Time
+		Matchername  string
 		CachedValues []CacheStorage
 	}{
-		Timestamp:   time.Now(),
-		Matchername: generatedMatcherName,
+		Timestamp:    time.Now(),
+		Matchername:  generatedMatcherName,
 		CachedValues: cachedValues,
 	})
-
+	die(err)
 }
