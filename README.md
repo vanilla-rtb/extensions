55d9c1c2093ade50099dde940d2450466d8748b1
venediktov <vvenedict@gmail.com>
2018-02-14 21:53:12 -0800

    finish code generator

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ to execute matcher cache genertor run following command
 ```bash
 git get github.com/jessevdk/go-flags
 cd golang
-go run  bidder_generator.go --output domain.hpp --input matcher.tmpl
+go run  bidder_generator.go --output-dir . --input-template matcher.tmpl
 ```
--- golang/bidder_generator.go
+++ golang/bidder_generator.go
@@ -7,49 +7,92 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
-	"path/filepath"
-	"reflect"
-	"strings"
-	"text/template"
-	"time"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "os"
+    _ "path/filepath"
+    "reflect"
+    "sort"
+    "strings"
+    "text/template"
+    "time"
 
-	"github.com/jessevdk/go-flags"
+    "github.com/jessevdk/go-flags"
 )
 
 //TODO: use reflection to create a list of fields in the template, How do we pass this struct to generator ???
 type Domain struct {
-	name   string `cpp:"std::string"  ipc:"char_string""`
-	dom_id uint32 `cpp:"uint32_t" ipc:"uint32_t"`
+    name      string `cpp:"std::string"  ipc:"char_string" is_key:"yes"`
+    domain_id uint32 `cpp:"uint32_t" ipc:"uint32_t"`
 }
 
-type CacheStorage struct {
-	FieldName    string
-	FieldType    string
-	FieldTypeIPC string
+type CacheEntity struct {
+    FieldName    string
+    FieldType    string
+    FieldTypeIPC string
+    IsKey        bool
+}
+
+type CacheEntities []CacheEntity
+
+func (s CacheEntities) Len() int {
+	return len(s)
+}
+
+func (s CacheEntities) Less(i, j int) bool {
+	return bool2int8(s[i].IsKey) < bool2int8(s[j].IsKey)
+}
+
+func (s CacheEntities) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func bool2int8 ( b bool ) int8 {
+	var bits int8 = 0
+	if b {
+		bits = 1
+	}
+	return bits
 }
 
 func die(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
+    if err != nil {
+        log.Fatal(err)
+    }
 }
 
 var funcMap = template.FuncMap{
-	"ToUpper": strings.ToUpper,
-	"ToLower": strings.ToLower,
-	"Title":   strings.Title,
-	"NotLast": func(x int, a interface{}) bool {
-		return x != reflect.ValueOf(a).Len()-1
+    "ToUpper": strings.ToUpper,
+    "ToLower": strings.ToLower,
+    "Title":   strings.Title,
+    "NotLast": func(x int, a interface{}) bool {
+        return x != reflect.ValueOf(a).Len()-1
+    },
+	"IsKey": func(storage CacheEntity) bool {
+		return storage.IsKey
+	},
+	"IsIPC" : func(storage CacheEntity) bool {
+	    return storage.FieldType != storage.FieldTypeIPC
+	},
+	"GetCacheKeys" : func(storage CacheEntities) CacheEntities {
+		keys := []CacheEntity{}
+		for i := range storage {
+			if(storage[i].IsKey) {
+				keys = append(keys, storage[i])
+			}
+		}
+		return keys
+	},
+	"Sort" : func(storage CacheEntities) CacheEntities {
+		sort.Reverse(storage)
+		return storage
 	},
 }
 
 type Options struct {
-	Input  flags.Filename `short:"i" long:"input" description:"Input file" default:"-"`
-	Output flags.Filename `short:"o" long:"output" description:"Output file" default:"-"`
+    InputTemplate flags.Filename `short:"i" long:"input-template" description:"InputTemplate file" default:"-"`
+    OutputDir     flags.Filename `short:"o" long:"output-dir" description:"OutputDir file" default:"-"`
 }
 
 var options Options
@@ -57,41 +100,44 @@ var parser = flags.NewParser(&options, flags.Default)
 
 func main() {
 
-	_, err := parser.Parse()
-	die(err)
+    _, err := parser.Parse()
+    die(err)
 
-	templateContent, err := ioutil.ReadFile(string(options.Input))
-	die(err)
-	var matcherTemplate = template.Must(template.New("").Funcs(funcMap).Parse(string(templateContent)))
+    templateContent, err := ioutil.ReadFile(string(options.InputTemplate))
+    die(err)
+    var matcherTemplate = template.Must(template.New("").Funcs(funcMap).Parse(string(templateContent)))
 
-	domain := Domain{"", 0}
-	v := reflect.ValueOf(domain)
+    domain := Domain{"", 0}
+    v := reflect.ValueOf(domain)
 
-	cachedValues := make([]CacheStorage, v.NumField())
+    cachedValues := make([]CacheEntity, v.NumField())
 
-	for i := 0; i < v.NumField(); i++ {
-		field := v.Type().Field(i)
-		cachedValues[i].FieldType = field.Tag.Get("cpp")
-		cachedValues[i].FieldTypeIPC = field.Tag.Get("ipc")
-		cachedValues[i].FieldName = field.Name
-		fmt.Println(cachedValues[i].FieldName, "=>", cachedValues[i].FieldType, "=>", cachedValues[i].FieldTypeIPC)
-	}
+    for i := 0; i < v.NumField(); i++ {
+        field := v.Type().Field(i)
+        cachedValues[i].FieldType = field.Tag.Get("cpp")
+        cachedValues[i].FieldTypeIPC = field.Tag.Get("ipc")
+        cachedValues[i].FieldName = field.Name
+        is_key_str := field.Tag.Get("is_key")
+		cachedValues[i].IsKey = len(is_key_str) > 0 && is_key_str=="yes"
+        fmt.Println(cachedValues[i].FieldName, "=>", cachedValues[i].FieldType, "=>", cachedValues[i].FieldTypeIPC, "=>", cachedValues[i].IsKey)
+    }
 
-	outFileName := string(options.Output)
-	generatedMatcherName := strings.TrimSuffix(outFileName, filepath.Ext(outFileName))
+	generatedMatcherName := strings.ToLower(v.Type().Name())
+    outFileName := strings.Join([]string{string(options.OutputDir) , strings.Join([]string{generatedMatcherName,".hpp"},"")} , "/")
+    //generatedMatcherName := strings.TrimSuffix(outFileName, filepath.Ext(outFileName))
 
-	f, err := os.Create(outFileName)
-	die(err)
-	defer f.Close()
+    f, err := os.Create(outFileName)
+    die(err)
+    defer f.Close()
 
-	err = matcherTemplate.Execute(f, struct {
-		Timestamp    time.Time
-		Matchername  string
-		CachedValues []CacheStorage
-	}{
-		Timestamp:    time.Now(),
-		Matchername:  generatedMatcherName,
-		CachedValues: cachedValues,
-	})
-	die(err)
+    err = matcherTemplate.Execute(f, struct {
+        Timestamp    time.Time
+        Matchername  string
+        CachedValues []CacheEntity
+    }{
+        Timestamp:    time.Now(),
+        Matchername:  generatedMatcherName,
+        CachedValues: cachedValues,
+    })
+    die(err)
 }
