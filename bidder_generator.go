ad416727b7112b87af1c7a86fff4bbb92af3dd91
venediktov <vvenedict@gmail.com>
2018-02-25 22:35:35 -0800

    utilize flags function callback to select generator

--- bidder_generator.go
+++ bidder_generator.go
@@ -17,6 +17,7 @@ import (
 
     "github.com/jessevdk/go-flags"
     "github.com/vanilla-rtb/extensions/codegen"
+    "reflect"
 )
 
 func die(err error) {
@@ -28,20 +29,25 @@ func die(err error) {
 type Options struct {
     InputTemplate flags.Filename `short:"i" long:"input-template" description:"InputTemplate file" default:"-"`
     OutputDir     flags.Filename `short:"o" long:"output-dir" description:"OutputDir file" default:"-"`
-    GeneratorType flags.Filename `short:"g" long:"generator-type" description:"GeneratorType" default:"cache"`
+    GeneratorType func(string) `short:"g" long:"generator-type" description:"GeneratorType" default:"cache"`
 }
 
 var options Options
 var parser = flags.NewParser(&options, flags.Default)
 
-//var generatorTypes = map[string]reflect.Type {
-//    "cmake"  : reflect.ValueOf(codegen.CmakeGenerator{}).Type(),
-//    "app"    : reflect.ValueOf(codegen.AppGenerator{}).Type(),
-//    "cache"  : reflect.ValueOf(codegen.CacheGenerator{}).Type(),
-//}
+var generatorTypes = map[string]reflect.Type {
+    "cmake"  : reflect.TypeOf(codegen.CmakeGenerator{}),
+    "app"    : reflect.TypeOf(codegen.AppGenerator{}),
+    "cache"  : reflect.TypeOf(codegen.CacheGenerator{}),
+}
+
+var generatorType reflect.Type
 
 func main() {
 
+    options.GeneratorType = func(gen string) {
+      generatorType = generatorTypes[gen]
+    }
     _, err := parser.Parse()
     die(err)
 
@@ -49,16 +55,16 @@ func main() {
     die(err)
 
     var Template= template.Must(template.New("").Funcs(codegen.FuncMap).Parse(string(templateContent)))
-    if options.GeneratorType == "cache" {
+    if generatorType == reflect.TypeOf(codegen.CacheGenerator{}) {
         err := codegen.NewCacheGenerator(options.OutputDir, Template).Execute(nil)
         die(err)
-    } else if options.GeneratorType == "app" {
+    } else if generatorType == reflect.TypeOf(codegen.AppGenerator{}) {
         outFileName := strings.Join([]string{string(options.OutputDir), "bidder", ".cpp"}, "/")
         f, err := os.Create(outFileName)
         die(err)
         err = codegen.NewAppGenerator(Template).Execute(f)
         die(err)
-    } else if options.GeneratorType == "cmake" {
+    } else if generatorType == reflect.TypeOf(codegen.CmakeGenerator{}) {
         outFileName := strings.Join([]string{string(options.OutputDir), "CMakeFile.txt"}, "/")
         f, err := os.Create(outFileName)
         die(err)
@@ -66,17 +72,5 @@ func main() {
         die(err)
     }
 
-    //for _, value := range stubs.TypeRegistry {
-    //    gen := codegen.NewEntityGenerator(reflect.New(value).Elem().Interface(), matcherTemplate)
-	//
-	//   outFileName := strings.Join([]string{string(options.OutputDir), strings.Join([]string{strings.ToLower(gen.GeneratedBasicName), ".hpp"}, "")}, "/")
-	//
-    //    f, err := os.Create(outFileName)
-    //    die(err)
-    //    defer f.Close()
-	//
-    //    //Generate code for each stub
-    //    err = gen.Execute(f)
-    //    die(err)
-    //}
+
 }
