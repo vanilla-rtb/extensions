1fb68ae17846b88be7124ba0b60cba10a30fc668
abushev <arseny.bushev@gmail.com>
2018-02-27 23:44:07 +0300

    some refactoring

--- bidder_generator.go
+++ bidder_generator.go
@@ -7,70 +7,69 @@
 package main
 
 import (
-    "io/ioutil"
-    "os"
-    "strings"
-    "text/template"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"text/template"
 
-    "log"
-    //"reflect"
-
-    "github.com/jessevdk/go-flags"
-    "github.com/vanilla-rtb/extensions/codegen"
-    "reflect"
+	"github.com/jessevdk/go-flags"
+	"github.com/vanilla-rtb/extensions/codegen"
 )
 
 func die(err error) {
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 type Options struct {
-    InputTemplate flags.Filename `short:"i" long:"input-template" description:"InputTemplate file" default:"-"`
-    OutputDir     flags.Filename `short:"o" long:"output-dir" description:"OutputDir file" default:"-"`
-    GeneratorType func(string) `short:"g" long:"generator-type" description:"GeneratorType" default:"cache"`
+	InputTemplate flags.Filename `short:"i" long:"input-template" description:"InputTemplate file" default:"-"`
+	OutputDir     flags.Filename `short:"o" long:"output-dir" description:"OutputDir file" default:"-"`
+	GeneratorType func(string)   `short:"g" long:"generator-type" description:"GeneratorType" default:"cache"`
 }
 
 var options Options
 var parser = flags.NewParser(&options, flags.Default)
 
-var generatorTypes = map[string]reflect.Type {
-    "cmake"  : reflect.TypeOf(codegen.CmakeGenerator{}),
-    "app"    : reflect.TypeOf(codegen.AppGenerator{}),
-    "cache"  : reflect.TypeOf(codegen.CacheGenerator{}),
+type GenerateFunc func(*Options, *template.Template) error
+
+var generatorTypes = map[string]GenerateFunc{
+	"cmake": func(o *Options, t *template.Template) error {
+		outFileName := strings.Join([]string{string(options.OutputDir), "CMakeFile.txt"}, "/")
+		f, err := os.Create(outFileName)
+		if err != nil {
+			return err
+		}
+		return codegen.NewCmakeGenerator("bidder", t).Execute(f)
+	},
+	"app": func(o *Options, t *template.Template) error {
+		outFileName := strings.Join([]string{string(options.OutputDir), "bidder", ".cpp"}, "/")
+		f, err := os.Create(outFileName)
+		if err != nil {
+			return err
+		}
+		return codegen.NewAppGenerator(t).Execute(f)
+	},
+	"cache": func(o *Options, t *template.Template) error {
+		return codegen.NewCacheGenerator(options.OutputDir, t).Execute(nil)
+	},
 }
 
-var generatorType reflect.Type
-
 func main() {
+	var generateFunc GenerateFunc
+	options.GeneratorType = func(gen string) {
+		var ok bool
+		if generateFunc, ok = generatorTypes[gen]; !ok {
+			log.Fatalf("Unsupported generator-type %s\n", gen)
+		}
+	}
+	_, err := parser.Parse()
+	die(err)
+	templateContent, err := ioutil.ReadFile(string(options.InputTemplate))
+	die(err)
 
-    options.GeneratorType = func(gen string) {
-      generatorType = generatorTypes[gen]
-    }
-    _, err := parser.Parse()
-    die(err)
-
-    templateContent, err := ioutil.ReadFile(string(options.InputTemplate))
-    die(err)
-
-    var Template= template.Must(template.New("").Funcs(codegen.FuncMap).Parse(string(templateContent)))
-    if generatorType == reflect.TypeOf(codegen.CacheGenerator{}) {
-        err := codegen.NewCacheGenerator(options.OutputDir, Template).Execute(nil)
-        die(err)
-    } else if generatorType == reflect.TypeOf(codegen.AppGenerator{}) {
-        outFileName := strings.Join([]string{string(options.OutputDir), "bidder", ".cpp"}, "/")
-        f, err := os.Create(outFileName)
-        die(err)
-        err = codegen.NewAppGenerator(Template).Execute(f)
-        die(err)
-    } else if generatorType == reflect.TypeOf(codegen.CmakeGenerator{}) {
-        outFileName := strings.Join([]string{string(options.OutputDir), "CMakeFile.txt"}, "/")
-        f, err := os.Create(outFileName)
-        die(err)
-        err = codegen.NewCmakeGenerator("bidder",Template).Execute(f)
-        die(err)
-    }
-
-
+	var tmpl = template.Must(template.New("").Funcs(codegen.FuncMap).Parse(string(templateContent)))
+	err = generateFunc(&options, tmpl)
+	die(err)
 }
