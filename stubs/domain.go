69c5f70a8bfb0f88240152e0c0c125dbe0571709
venediktov <vvenedict@gmail.com>
2018-03-31 23:58:07 -0700

    multi key support

--- stubs/domain.go
+++ stubs/domain.go
@@ -14,10 +14,25 @@ type ICOCampaign struct {
     campaign_id uint32 `cpp:"uint32_t" ipc:"uint32_t"  is_value:"yes"`
 }
 
+
+type Geo struct {
+ city string `cpp:"std::string"  ipc:"char_string" is_key:"yes"`
+ country string `cpp:"std::string"  ipc:"char_string" is_key:"yes"`
+ geo_id uint32 `cpp:"uint32_t" ipc:"uint32_t" is_value:"yes"`
+}
+
+type GeoCampaign struct {
+    geo_id      uint32 `cpp:"uint32_t" ipc:"uint32_t" is_key:"yes"`
+    campaign_id uint32 `cpp:"uint32_t" ipc:"uint32_t" is_value:"yes"`
+}
+
+
 //register all types for generator even unrelated can go all in one registry
 var TypeRegistry = []reflect.Type{
     reflect.ValueOf(Domain{}).Type(),
     reflect.ValueOf(ICOCampaign{}).Type(),
+    reflect.ValueOf(Geo{}).Type(),
+    reflect.ValueOf(GeoCampaign{}).Type(),
 }
 
 //agregate  targetings based on the bidder model the execution in the bidder will preserve as order of declaration
@@ -25,5 +40,9 @@ var Targetings = map[string][]reflect.Type{
     "ico": []reflect.Type{
         reflect.ValueOf(Domain{}).Type(),
         reflect.ValueOf(ICOCampaign{}).Type(),
+    },
+    "geo": []reflect.Type{
+        reflect.ValueOf(Geo{}).Type(),
+        reflect.ValueOf(GeoCampaign{}).Type(),
     },
 }
