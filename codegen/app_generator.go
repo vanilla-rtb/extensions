04d54965f3b1f5321d9139337113e245d2b1b21a
Arseny Bushev <abushev@MacBook-Pro-Arseny.local>
2024-02-27 17:02:23 +0300

    go.mod and appropriate changes for easy execution added

--- codegen/app_generator.go
+++ codegen/app_generator.go
@@ -1,49 +1,49 @@
 package codegen
 
 import (
-    "fmt"
-    "os"
-    "path"
-    "reflect"
-    "strings"
-    "stubs"
-    "text/template"
-    "time"
+	"fmt"
+	"github.com/vanilla-rtb/extensions/stubs"
+	"os"
+	"path"
+	"reflect"
+	"strings"
+	"text/template"
+	"time"
 )
 
 type AppGenerator struct {
-    OutputDir      string
-    MatcherDir     string
-    AppName        string
-    TargetingModel string
-    BuildType      string
-    Template       *template.Template
+	OutputDir      string
+	MatcherDir     string
+	AppName        string
+	TargetingModel string
+	BuildType      string
+	Template       *template.Template
 }
 
 func (appGen *AppGenerator) Execute() error {
-    outFileName := path.Join(appGen.OutputDir, strings.ToLower(appGen.AppName)+".cpp")
-    f, err := os.Create(outFileName)
-    if err != nil {
-        return err
-    }
-    defer f.Close()
+	outFileName := path.Join(appGen.OutputDir, strings.ToLower(appGen.AppName)+".cpp")
+	f, err := os.Create(outFileName)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
 
-    var cachedEntities []reflect.Type
-    var ok bool
-    if cachedEntities, ok = stubs.Targetings[appGen.TargetingModel]; !ok {
-        return fmt.Errorf("Failed to find TargetingModel %s", appGen.TargetingModel)
-    }
+	var cachedEntities []reflect.Type
+	var ok bool
+	if cachedEntities, ok = stubs.Targetings[appGen.TargetingModel]; !ok {
+		return fmt.Errorf("Failed to find TargetingModel %s", appGen.TargetingModel)
+	}
 
-    err = appGen.Template.Execute(f, struct {
-        Timestamp      time.Time
-        CachedEntities []reflect.Type
-        BidderName     string
-        BuildType      string
-    }{
-        Timestamp:      time.Now(),
-        CachedEntities: cachedEntities,
-        BidderName:     appGen.AppName,
-        BuildType:      appGen.BuildType,
-    })
-    return err
+	err = appGen.Template.Execute(f, struct {
+		Timestamp      time.Time
+		CachedEntities []reflect.Type
+		BidderName     string
+		BuildType      string
+	}{
+		Timestamp:      time.Now(),
+		CachedEntities: cachedEntities,
+		BidderName:     appGen.AppName,
+		BuildType:      appGen.BuildType,
+	})
+	return err
 }
--- codegen/cache_generator.go
+++ codegen/cache_generator.go
@@ -1,36 +1,36 @@
 package codegen
 
 import (
-    "os"
-    "path"
-    "reflect"
-    "strings"
-    "stubs"
-    "text/template"
+	"github.com/vanilla-rtb/extensions/stubs"
+	"os"
+	"path"
+	"reflect"
+	"strings"
+	"text/template"
 )
 
 type CacheGenerator struct {
-    OutputDir string
-    Template  *template.Template
+	OutputDir string
+	Template  *template.Template
 }
 
 func (cacheGen *CacheGenerator) Execute() error {
-    for _, value := range stubs.TypeRegistry {
-        entityGen := NewEntityGenerator(reflect.New(value).Elem().Interface(), cacheGen.Template)
+	for _, value := range stubs.TypeRegistry {
+		entityGen := NewEntityGenerator(reflect.New(value).Elem().Interface(), cacheGen.Template)
 
-        outFileName := path.Join(cacheGen.OutputDir, strings.ToLower(entityGen.GeneratedBasicName)+".hpp")
+		outFileName := path.Join(cacheGen.OutputDir, strings.ToLower(entityGen.GeneratedBasicName)+".hpp")
 
-        f, err := os.Create(outFileName)
-        if err != nil {
-            return err
-        }
-        defer f.Close()
+		f, err := os.Create(outFileName)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
 
-        //Generate code for each stub
-        err = entityGen.Execute(f)
-        if err != nil {
-            return err
-        }
-    }
-    return nil
+		//Generate code for each stub
+		err = entityGen.Execute(f)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
